3fba773cd769d48a68b05e132af29677f7f61a67
snoow-sub <yuuki.takagi98@gmail.com>
2024-11-21 16:21:35 +0900

    情報入力のガワを作成
    Co-authored-by: snoow-sub <snoow-sub@users.noreply.github.com>
    Co-authored-by: noda024 <noda024@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,55 +51,81 @@ export default function App() {
           placeholder="お名前を入力してください"
           required
         />
-
-        <label for="email">メールアドレス:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="example@example.com"
-          required
-        />
-
-        <label for="password">パスワード:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="パスワードを入力してください"
-          required
-        />
-
+        
         <label for="gender">性別:</label>
-        <select id="gender" name="gender" required>
-          <option value="">選択してください</option>
-          <option value="male">男性</option>
-          <option value="female">女性</option>
-          <option value="other">その他</option>
+        <select id="gender" name="gender" required><br />
+          <option value="">選択してください</option><br />
+          <option value="male">男性</option><br />
+          <option value="female">女性</option><br />
+          <option value="other">その他</option><br />
         </select>
+        
+        <br />
+        
+        <label for="age">年齢:</label>
+        <input
+          type="age"
+          id="age"
+          name="age"
+          placeholder="20"
+          required
+        />
 
-        <label>趣味:</label>
+        <br />
+        <label for="address">住所:</label>
+        <input
+          type="address"
+          id="address"
+          name="address"
+          placeholder="東京都XXXXXXXX"
+          required
+        />
+        
+        <label>趣味:</label> <br />
         <label>
-          <input type="checkbox" name="hobbies" value="sports" /> スポーツ
+          <input type="checkbox" name="hobbies" value="sports" /> スポーツ <br />
         </label>
         <label>
-          <input type="checkbox" name="hobbies" value="reading" /> 読書
+          <input type="checkbox" name="hobbies" value="reading" /> 読書 <br />
         </label>
         <label>
-          <input type="checkbox" name="hobbies" value="music" /> 音楽鑑賞
+          <input type="checkbox" name="hobbies" value="music" /> 音楽鑑賞 <br />
         </label>
         <label>
-          <input type="checkbox" name="hobbies" value="travel" /> 旅行
+          <input type="checkbox" name="hobbies" value="travel" /> 旅行<br />
         </label>
 
-        <label for="comments">コメント:</label>
-        <textarea
-          id="comments"
-          name="comments"
-          placeholder="自由にコメントをお書きください"
-          rows="5"
-        ></textarea>
+        <label for="name">どこで:</label>
+      <input
+        type="text"
+        id="where"
+        name="where"
+        placeholder="取り組みたい場所を選択してください"
+        required/>
+        <br></br>
 
+        <label for="inout"></label>
+        インドア派
+        <input 
+          type="range"
+          name="inout"
+          min="0"
+          max="100"
+        /> 
+        アウトドア派<br />
+
+        <label>規模感</label>
+        小
+        <input type="range" name="scale" min="0" max="100" step="5"/> 大<br />
+        
+        <label>距離の近さ</label>
+        近い方が良い
+        <input type="range" name="distance" min="0" max="100" step="5"/> 遠くても良い<br />
+
+        <label>静かさ</label>
+        黙々とやりたい
+        <input type="range" name="silent" min="0" max="100" step="5" /> 和気藹々とやりたい<br />
+        
         <button type="submit">送信</button>
       </form>
     </div>
