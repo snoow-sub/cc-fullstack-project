07bcb0799b41e426395984fee002ae9cd9340552
Ryo Nonoyama <ryounonoyama@gmail.com>
2024-11-23 16:18:26 +0900

    不要なファイルを削除

--- backend/routes/dishRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const dishController = require('../controllers/dishController')
-
-router.get('/', dishController.getAllDiners)
-
-module.exports = router
