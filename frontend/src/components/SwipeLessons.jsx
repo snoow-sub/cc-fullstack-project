3d67b0d641ef7460292cf1b284d6f3d7504b0342
tomoya noda <tom@tomoyanoMacBook-Air.local>
2024-11-24 16:11:37 +0900

    左フリックで次の画像に

--- frontend/src/components/SwipeLessons.jsx
+++ frontend/src/components/SwipeLessons.jsx
@@ -4,16 +4,17 @@ import "../css/swipe.css";
 
 export function SwipeLessons({ profile, setFlick, handleSwipeType, lesson }) {
   const currentPath = process.env.REACT_APP_BASE_DIR;
+  const [number, setNumber] = useState(0);
 
   const handlers = useSwipeable({
     onSwipedLeft: () => {
       handleSwipeType("left");
       // setFlick(false);
-      // console.log("呼ばれてますよ左");
-      // setTimeout(() => {
-      //   setFlick(true);
-      //   handleSwipeType("なし");
-      // }, 1000);
+      setNumber(number + 1);
+      console.log("呼ばれてますよ左");
+      setTimeout(() => {
+        handleSwipeType("なし");
+      }, 10);
     },
     onSwipedRight: () => {
       handleSwipeType("right");
@@ -25,6 +26,52 @@ export function SwipeLessons({ profile, setFlick, handleSwipeType, lesson }) {
       // }, 1000);
     },
   });
+
+  const mockData = [
+    {
+      date: "2024-12-02T15:00:00.000Z",
+      description: "テニスを一緒にしませんか？",
+      end_time: "20:00:00",
+      id: 3,
+      imagePath: "/image/tennis.png",
+      indicator: 92.1,
+      location: "関東",
+      momentum: null,
+      moviePath: "/movies/lesson3.mp4",
+      review: 4,
+      start_time: "18:00:00",
+      store_id: 1,
+    },
+    {
+      date: "2024-12-02T15:00:00.000Z",
+      description: "上級者向けヨガレッスン",
+      end_time: "20:00:00",
+      id: 3,
+      imagePath: "./image/yoga.png",
+      indicator: 92.1,
+      location: "関東",
+      momentum: null,
+      moviePath: "/movies/lesson3.mp4",
+      review: 4,
+      start_time: "18:00:00",
+      store_id: 1,
+    },
+    {
+      date: "2024-12-02T15:00:00.000Z",
+      description:
+        "一緒に生け花で遊びませんか?簡単なキットをお送りしますので、当日はオンラインでご参加いただけます！ 講師Rena",
+      end_time: "20:00:00",
+      id: 3,
+      imagePath: "./image/ike.png",
+      indicator: 92.1,
+      location: "関東",
+      momentum: null,
+      moviePath: "/movies/lesson3.mp4",
+      review: 4,
+      start_time: "18:00:00",
+      store_id: 1,
+    },
+  ];
 
   return (
     <>
@@ -45,20 +92,21 @@ export function SwipeLessons({ profile, setFlick, handleSwipeType, lesson }) {
       <div className="activity-box" {...handlers}>
         <img
           className="activity-image"
-          src="./image/ike.png"
+          src={mockData[number].imagePath}
           alt="生け花"
           {...handlers}
         />
         <div className="activity-details">
           <p>{profile.calendar}</p>
-          <p>生け花教室</p>
+          {/* <p>生け花教室</p> */}
           {/* <p>講座番号：{lesson[0][0].store_id}</p> */}
-          <p>一緒に生け花で遊びませんか？</p>
+          <p>{mockData[number].description}</p>
+          {/* <p>一緒に生け花で遊びませんか？</p>
           <p>
             簡単なキットをお送りしますので、当日はオンラインでご参加いただけます！
           </p>
           <p>興味がある方は一緒に生花について学びましょう！</p>
-          <p>講師：Rena</p>
+          <p>講師：Rena</p> */}
         </div>
       </div>
     </>
