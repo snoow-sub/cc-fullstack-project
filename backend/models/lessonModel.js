0081f72053073fe253efe49d32f179caa22cb9b5
Kota <knrecruit224@gmail.com>
2024-11-25 18:57:46 +0900

    /api/user/1/lessonにdate型を渡せるように修正&/api/lesson/popularを新規実装&テストをリッチに

--- backend/models/lessonModel.js
+++ backend/models/lessonModel.js
@@ -2,11 +2,42 @@ const knex = require("../knexConfig");
 
 module.exports = {
   async getLessons(location, startDate, endDate) {
-    const lessons = await knex("lesson") //startDate、endDate考慮。locationはあとで。indicatorは今後の展望
+    if (location === "特になし") {
+      const lessons = await knex("lesson")
+        .select("*")
+        .andWhere("date", ">=", startDate)
+        .andWhere("date", "<=", endDate);
+      return lessons;
+    }
+
+    const lessons = await knex("lesson")
       .select("*")
       .where("location", location)
       .andWhere("date", ">=", startDate)
       .andWhere("date", "<=", endDate);
+    return lessons;
+  },
+
+  async getPopularLessons(location, startDate, endDate) {
+    if (location === "特になし") {
+      const lessons = await knex("lesson")
+        .select("*")
+        .andWhere("date", ">=", startDate)
+        .andWhere("date", "<=", endDate)
+        .orderByRaw("review DESC NULLS LAST") // review を降順でソートし、NULL を最後に配置(ordarbyと書くと、DBの仕様によってはnullが最大値として扱われてしまうためこのような書き方)
+        .orderBy("date", "asc") //同じreviewが複数あった場合は日程順で取得。これがいいかは怪しい。indicatorを使うのがよいか
+        .limit(3);
+      return lessons;
+    }
+
+    const lessons = await knex("lesson")
+      .select("*")
+      .where("location", location)
+      .andWhere("date", ">=", startDate)
+      .andWhere("date", "<=", endDate)
+      .orderByRaw("review DESC NULLS LAST") // review を降順でソートし、NULL を最後に配置(ordarbyと書くと、DBの仕様によってはnullが最大値として扱われてしまうためこのような書き方)
+      .orderBy("date", "asc") //同じreviewが複数あった場合は日程順で取得。これがいいかは怪しい。indicatorを使うのがよいか
+      .limit(3);
     return lessons;
   },
 
