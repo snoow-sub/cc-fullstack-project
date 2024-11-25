011a0726a51da99d0138ecc9ce851c95dd4b495c
snoow-sub <yuuki.takagi98@gmail.com>
2024-11-26 08:07:48 +0900

    人気ページを作成、テストデータはApp.jsxに移動

--- frontend/src/components/SwipeLessons.jsx
+++ frontend/src/components/SwipeLessons.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useSwipeable } from "react-swipeable";
 import "../css/swipe.css";
 
-export function SwipeLessons({ profile, setFlick, reserveLesson, handleSwipeType, lesson }) {
+export function SwipeLessons({ profile, setFlick, reserveLesson, handleSwipeType, lesson, popularLesson }) {
   const currentPath = process.env.REACT_APP_BASE_DIR || "../../";
   const [number, setNumber] = useState(0);
   const limit = 5;
@@ -10,9 +10,10 @@ export function SwipeLessons({ profile, setFlick, reserveLesson, handleSwipeType
   const handlers = useSwipeable({
     onSwipedLeft: () => {
       handleSwipeType("left");
-      // setFlick(false);
-      reserveLesson(number + 1);
-      setNumber(number + 1);
+      if (number < limit) {
+        reserveLesson(number + 1);
+        setNumber(number + 1);
+      }
       console.log("呼ばれてますよ左");
       setTimeout(() => {
         handleSwipeType("なし");
@@ -31,118 +32,78 @@ export function SwipeLessons({ profile, setFlick, reserveLesson, handleSwipeType
     },
   });
 
-  //デモ用データ
-  const mockData = [
-    {
-      date: "2024-12-02T15:00:00.000Z",
-      description: "テニスを一緒にしませんか？",
-      end_time: "20:00:00",
-      id: 1,
-      imagePath: "/image/tennis_1.gif",
-      indicator: 92.1,
-      location: "関東",
-      momentum: null,
-      moviePath: "./movie/lesson3.mp4",
-      review: 4,
-      start_time: "18:00:00",
-      store_id: 1,
-    },
-    {
-      date: "2024-12-02T15:00:00.000Z",
-      description: "ヨガや瞑想の基本から学びましょう！",
-      end_time: "20:00:00",
-      id: 2,
-      imagePath: "./image/yoga_1.gif",
-      indicator: 92.1,
-      location: "関東",
-      momentum: null,
-      moviePath: "./movie/lesson3.mp4",
-      review: 4,
-      start_time: "18:00:00",
-      store_id: 1,
-    },
-    {
-      date: "2024-12-02T15:00:00.000Z",
-      description:
-        "一緒に生け花で遊びませんか?簡単なキットをお送りしますので、当日はオンラインでご参加いただけます！",
-      end_time: "20:00:00",
-      id: 3,
-      imagePath: "./image/hana_1.gif",
-      indicator: 92.1,
-      location: "関東",
-      momentum: null,
-      moviePath: "./movie/lesson3.mp4",
-      review: 4,
-      start_time: "18:00:00",
-      store_id: 1,
-    },
-    {
-      date: "2024-12-02T15:00:00.000Z",
-      description: "テニスを一緒にしませんか？",
-      end_time: "20:00:00",
-      id: 4,
-      imagePath: "/image/tennis.png",
-      indicator: 92.1,
-      location: "関東",
-      momentum: null,
-      moviePath: "./movie/lesson3.mp4",
-      review: 4,
-      start_time: "18:00:00",
-      store_id: 1,
-    },
-    {
-      date: "2024-12-02T15:00:00.000Z",
-      description: "ヨガや瞑想の基本から学びましょう！",
-      end_time: "20:00:00",
-      id: 5,
-      imagePath: "./image/yoga.png",
-      indicator: 92.1,
-      location: "関東",
-      momentum: null,
-      moviePath: "./movie/lesson3.mp4",
-      review: 4,
-      start_time: "18:00:00",
-      store_id: 1,
-    },
-  ];
+  function clickPopularLesson(popularNumber) {
+    setNumber(popularNumber);
+    reserveLesson(popularNumber);
+    setFlick(false);
+  }
 
   return (
     <>
-      <div className="profile-info">
-        <center>
-          <form className="set-calendar">
-            <label className="calendar-design">
-              <input type="date" />
-            </label>
-          </form>
-        </center>
-        <br />
-      </div>
-      <br />
       { number < limit ? (
+      <div>
+        <div className="profile-info">
+          <center>
+            <form className="set-calendar">
+              <label className="calendar-design">
+                <input type="date" />
+              </label>
+            </form>
+          </center>
+          <br />
+        </div>
+        <br />
         <div className="lesson-box" {...handlers}>
           <img
             className="lesson-image"
-            src={mockData[number].imagePath}
+            src={lesson[number].imagePath}
             alt="生け花"
             {...handlers}
           />
           <div className="lesson-details">
             {/* <p>{profile.calendar}</p> */}
-            {/* <p>生け花教室</p> */}
-            {/* <p>講座番号：{lesson[0][0].store_id}</p> */}
-            <p>{mockData[number].description}</p>
-            {/* <p>一緒に生け花で遊びませんか？</p>
-            <p>
-              簡単なキットをお送りしますので、当日はオンラインでご参加いただけます！
-            </p>
-            <p>興味がある方は一緒に生花について学びましょう！</p>
-            <p>講師：Rena</p> */}
+            <p>レッスン内容：{lesson[number].description}</p>
+            <p>日時：{lesson[number].date}</p>
+            <p>開始予定時刻：{lesson[number].start_time}</p>
+            <p>終了予定時刻：{lesson[number].location}</p>
+            <p>場所：{lesson[number].location}</p>
           </div>
         </div>
+      </div>
       ) : (
-        <div className="lesson-box">
-          <p>これ以上はありません</p>
+        <div>
+          <div>
+            <center><h1>人気Top3のレッスン</h1></center>
+          </div>
+          <div className="lesson-box">
+            <div className="lesson-details" onClick={() => clickPopularLesson(0)}>
+              <p>レッスン内容：{popularLesson[0].description}</p>
+              <p>日時：{popularLesson[0].date}</p>
+              <p>開始予定時刻：{popularLesson[0].start_time}</p>
+              <p>終了予定時刻：{popularLesson[0].location}</p>
+              <p>場所：{popularLesson[0].location}</p>
+            </div>
+          </div>
+          <br />
+          <div className="lesson-box">
+            <div className="lesson-details" onClick={() => clickPopularLesson(1)}>
+              <p>レッスン内容：{popularLesson[1].description}</p>
+              <p>日時：{popularLesson[1].date}</p>
+              <p>開始予定時刻：{popularLesson[1].start_time}</p>
+              <p>終了予定時刻：{popularLesson[1].location}</p>
+              <p>場所：{popularLesson[1].location}</p>
+            </div>
+          </div>
+          <br />
+          <div className="lesson-box">
+            <div className="lesson-details" onClick={() => clickPopularLesson(2)}>
+              <p>レッスン内容：{popularLesson[2].description}</p>
+              <p>日時：{popularLesson[2].date}</p>
+              <p>開始予定時刻：{popularLesson[2].start_time}</p>
+              <p>終了予定時刻：{popularLesson[2].location}</p>
+              <p>場所：{popularLesson[2].location}</p>
+            </div>
+          </div>
         </div>
       )}
     </>
