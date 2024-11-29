d034a28f3f6a4df4042c1eeb0385668e03a5a53d
Kota <knrecruit224@gmail.com>
2024-11-29 10:12:34 +0900

    lessonの検索ロジックを修正

--- backend/db/seeds/20241122061914_initial_user.js
+++ backend/db/seeds/20241122061914_initial_user.js
@@ -12,7 +12,7 @@ exports.seed = async function (knex) {
     {
       id: 1,
       name: "山田 太郎",
-      sex: 1, // 男性
+      sex: 0, // 男性
       birthday: "1990-01-15", // 生年月日
       address: "東京都新宿区",
       indicator: 85.5, // 指標値
@@ -20,7 +20,7 @@ exports.seed = async function (knex) {
     {
       id: 2,
       name: "佐藤 花子",
-      sex: 2, // 女性
+      sex: 1, // 女性
       birthday: "1985-07-20",
       address: "大阪府大阪市中央区",
       indicator: 70.2, // 指標値
@@ -36,7 +36,7 @@ exports.seed = async function (knex) {
     {
       id: 4,
       name: "田中 一郎",
-      sex: 1, // 男性
+      sex: 0, // 男性
       birthday: "1995-05-25",
       address: "北海道札幌市中央区",
       indicator: 65.3,
@@ -44,7 +44,7 @@ exports.seed = async function (knex) {
     {
       id: 5,
       name: "高橋 美咲",
-      sex: 2, // 女性
+      sex: 1, // 女性
       birthday: "1998-11-12",
       address: "福岡県福岡市博多区",
       indicator: 78.8,
@@ -52,7 +52,7 @@ exports.seed = async function (knex) {
     {
       id: 6,
       name: "伊藤 勇",
-      sex: 1, // 男性
+      sex: 0, // 男性
       birthday: "1988-02-18",
       address: "神奈川県横浜市中区",
       indicator: 82.0,
@@ -60,7 +60,7 @@ exports.seed = async function (knex) {
     {
       id: 7,
       name: "渡辺 幸子",
-      sex: 2, // 女性
+      sex: 1, // 女性
       birthday: "1992-09-30",
       address: "愛知県名古屋市中区",
       indicator: 74.5,
@@ -68,7 +68,7 @@ exports.seed = async function (knex) {
     {
       id: 8,
       name: "中村 智子",
-      sex: 2, // 女性
+      sex: 1, // 女性
       birthday: "1975-03-15",
       address: "兵庫県神戸市中央区",
       indicator: 88.7,
@@ -76,7 +76,7 @@ exports.seed = async function (knex) {
     {
       id: 9,
       name: "小林 剛",
-      sex: 1, // 男性
+      sex: 0, // 男性
       birthday: "1982-06-28",
       address: "広島県広島市中区",
       indicator: 67.4,
@@ -84,7 +84,7 @@ exports.seed = async function (knex) {
     {
       id: 10,
       name: "松本 綾子",
-      sex: 2, // 女性
+      sex: 1, // 女性
       birthday: "1993-08-21",
       address: "沖縄県那覇市",
       indicator: 79.9,
